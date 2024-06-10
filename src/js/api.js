5cfb0c7f2416ec588194b957c5a1c4daee5a5c2c
Olena Pestova <nowalena9@gmail.com>
2024-06-10 20:43:00 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ src/js/api.js
@@ -0,0 +1,7 @@
+//get  list of genres https://api.themoviedb.org/3/genre/movie/list?language=en'
+//get  movie by id ttps://api.themoviedb.org/3/movie/{movie_id}
+
+//get movie by search https://api.themoviedb.org/3/search/movie 
+// params:query string required, language string, page string
+
+//string
