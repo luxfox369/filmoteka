e7ba13db88b6428cf3eeadab880dba92802167ae
Olena Pestova <nowalena9@gmail.com>
2024-06-14 14:05:55 +0300

    before change brain

--- src/index.js
+++ src/index.js
@@ -1,39 +1,19 @@
+
 import './js/swiper';
-<<<<<<< HEAD
-import './js/closeModal';
-import './js/loader';
-import './js/storage';
-import './js/serviseAPI';
-//import './js/api';
-import './js/onFirstRender';
-import './js/modal_footer';
-import './js/paginator';
-// import './js/btnUp';
-// import './js/queue';
-import './js/searchQuery';
-//import './js/top-slider';
-//import './js/sliderMarkup';
-=======
-import './js/top-slider';
-import './js/sliderMarkup';
 import './js/loader';
 import './js/storage';
 import './js/api';
+import './js/sliderMarkup';
 import './js/onFirstRender';
 import './js/paginator';
 import './js/btnUp';
 import './js/searchQuery';
->>>>>>> selector_language
+import './js/top-slider';
+
 import './js/modalMovie';
 import './js/modal_footer';
 import './js/closeModal';
 import './js/trailers';
-<<<<<<< HEAD
-import './js/modal_button_storage';
-// import './js/renderWatched';
-
-=======
-//import './js/modal_button_storage';
 import './js/watched';
 import './js/queue';
->>>>>>> selector_language
+
