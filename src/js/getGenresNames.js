8a091524d71bb0d58682a859e393b475ef8c47a1
Olena Pestova <nowalena9@gmail.com>
2024-06-15 00:26:24 +0300

    getCenresNames

--- src/js/getGenresNames.js
+++ src/js/getGenresNames.js
@@ -3,7 +3,7 @@ export const getGenresNames = ids => {
    const savedGenres = storage.loadGenres();
     let genresNames = Object.entries(savedGenres)
       .filter(([key]) => ids.includes(parseInt(key)))
-      .map(([_, value]) => value);
+      .map(([_, value]) => value.name);
       if(genresNames.length > 3) {
         return genresNames = [genresNames[0], genresNames[1], 'Other']
        }
